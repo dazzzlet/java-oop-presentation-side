0eb06a70affec710aa91397da22f8c42fd8c04c5
truongnln_net <nlnt@netcompany.com>
2023-02-01 09:36:27 +0000

    [TruongNLN] add index file to use this template as a library

---
+++ index.js
@@ -0,0 +1,7 @@
+export * from './src/components/image';
+export * from './src/layout/code-column-layout';
+export * from './src/layout/code-layout';
+export * from './src/layout/white-column-layout';
+export * from './src/layout/white-layout';
+export * from './src/layout/introduction';
+export { default as netcompanyTheme } from './src/themes/main';
