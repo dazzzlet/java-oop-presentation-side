6494e133d221c661cb724de8beeb2c7d7992988a
truongnln_net <nlnt@netcompany.com>
2023-02-03 14:37:29 +0000

    [TruongNLN] add mermaid viewer

--- src/components/index.js
+++ src/components/index.js
@@ -2,4 +2,5 @@ export * from './common';
 export * from './code-viewer';
 export * from './image';
 export * from './lister-steps';
+export * from './mermaid-viewer';
 export * from './switcher-steps';
---
+++ src/components/mermaid-viewer.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { useDeck } from "mdx-deck";
+import Mermaid from 'react-mermaid2';
+import styles from "./mermaid-viewer.css";
+import useSteps from "../utils/use-steps";
+import { getStepsFromChildren } from "./code-viewer";
+
+
+export function MermaidViewer({ children }) {
+    const deck = useDeck();
+    const steps = React.useMemo(getStepsFromChildren(children), [deck.index]);
+
+    const stepIndex = useSteps(steps.length - 1);
+
+    return (
+        <div className={styles.codeContainer}>
+            {steps.map((step, index) => (
+                <div key={index} style={{ maxHeight: '100%', height: '100%', visibility: (index != stepIndex) ? 'hidden' : undefined }}>
+                    <Mermaid chart={step.code} />
+                </div>
+            ))}
+        </div>
+    );
+}
